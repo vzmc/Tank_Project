f877089c662288d269f53e6930ac86df5ccc7bbb
zyb <zyb@aiming-inc.com>
2021-10-29 17:37:55 +0900

    update tank control

--- URP_Temp/Assets/Scripts/TankMoveController.cs
+++ URP_Temp/Assets/Scripts/TankMoveController.cs
@@ -15,22 +15,26 @@ public class TankMoveController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        var motorTorque = maxMotorTorque * currentAccelerateInputValue;
         var steerAngel = maxSteerAngel * currentTurnInputValue;
-        var breakTorque = Mathf.Abs(currentAccelerateInputValue) <= float.Epsilon ? maxBreakTorque : 0;
-
         foreach (var wheel in frontWheels)
         {
             wheel.steerAngle = steerAngel;
-            wheel.motorTorque = motorTorque;
-            wheel.brakeTorque = breakTorque;
+            UpdateWheel(wheel);
         }
         
         foreach (var wheel in backWheels)
         {
-            wheel.motorTorque = motorTorque;
-            wheel.brakeTorque = breakTorque;
+            UpdateWheel(wheel);
         }
+    }
+
+    private void UpdateWheel(WheelCollider wheel)
+    {
+        var motorTorque = currentAccelerateInputValue * wheel.rpm >= 0 ? maxMotorTorque * currentAccelerateInputValue : 0;
+        wheel.motorTorque = motorTorque;
+        
+        var breakTorque = currentAccelerateInputValue * wheel.rpm < -float.Epsilon ? maxBreakTorque * Mathf.Abs(currentAccelerateInputValue) : 0;
+        wheel.brakeTorque = breakTorque;
     }
 
     private void OnAccelerate(InputValue inputValue)
